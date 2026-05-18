af8ad3a66bb2f77bf08f806c29347b0991abffc5
abicoto24 <164296369+abicoto24@users.noreply.github.com>
2026-05-18 13:06:20 -0600

    Botones del menú funcionando

--- Tarea Programada 2/TP2.py
+++ Tarea Programada 2/TP2.py
@@ -1,15 +1,22 @@
-import funciones
+from funciones import *
 import tkinter as tk
 ventana = tk.Tk()
 ventana.title("Banco de Sangre")
 ventana.geometry("400x300")
 
 botonInsertarDonador = tk.Button(ventana, text = "Insertar donador", command = insertarDonador)
+botonInsertarDonador .pack()
 botonGenerarDonadores = tk.Button(ventana, text = "Generar donadores", command = generarDonadores)
+botonGenerarDonadores.pack()
 botonActualizarDatos = tk.Button(ventana, text = "Actualizar datos del donador", command = actualizarDatos)
+botonActualizarDatos.pack()
 botonEliminarDonador = tk.Button(ventana, text = "Eliminar donador", command = eliminarDonador)
+botonEliminarDonador.pack()
 botonInsertarLugar = tk.Button(ventana, text = "Insertar lugar de donación", command = insertarLugar)
+botonInsertarLugar.pack()
 botonReportes = tk.Button(ventana, text = "Reportes", command = reportes)
+botonReportes.pack()
 botonSalir = tk.Button(ventana, text = "Salir", command = salir) 
+botonSalir.pack()
 
 ventana.mainloop()
---
+++ Tarea Programada 2/funciones.py
@@ -0,0 +1,20 @@
+def insertarDonador():
+    print()
+
+def generarDonadores():
+    print()
+
+def actualizarDatos():
+    print()
+
+def eliminarDonador():
+    print()
+
+def insertarLugar():
+    print()
+
+def reportes():
+    print()
+
+def salir():
+    print()
