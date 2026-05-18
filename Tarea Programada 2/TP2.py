858617bdd0a2f23208d411a374bbde844c954dac
abicoto24 <164296369+abicoto24@users.noreply.github.com>
2026-05-18 12:56:59 -0600

    Corregí errores en el menú

--- Tarea Programada 2/TP2.py
+++ Tarea Programada 2/TP2.py
@@ -1,13 +1,15 @@
+import funciones
 import tkinter as tk
 ventana = tk.Tk()
 ventana.title("Banco de Sangre")
 ventana.geometry("400x300")
-ventana.mainloop()
 
-boton = tk.Button(ventana, text = "Insertar donador", command = insertarDonador())
-boton = tk.Button(ventana, text = "Generar donadores", command = generarDonadores())
-boton = tk.Button(ventana, text = "Actualizar datos del donador", command = actualizarDatos())
-boton = tk.Button(ventana, text = "Eliminar donador", command = eliminarDonador())
-boton = tk.Button(ventana, text = "Insertar lugar de donación", command = insertarLugar())
-boton = tk.Button(ventana, text = "Reportes", command = reportes())
-boton = tk.Button(ventana, text = "Salir", command = salir()) 
+botonInsertarDonador = tk.Button(ventana, text = "Insertar donador", command = insertarDonador)
+botonGenerarDonadores = tk.Button(ventana, text = "Generar donadores", command = generarDonadores)
+botonActualizarDatos = tk.Button(ventana, text = "Actualizar datos del donador", command = actualizarDatos)
+botonEliminarDonador = tk.Button(ventana, text = "Eliminar donador", command = eliminarDonador)
+botonInsertarLugar = tk.Button(ventana, text = "Insertar lugar de donación", command = insertarLugar)
+botonReportes = tk.Button(ventana, text = "Reportes", command = reportes)
+botonSalir = tk.Button(ventana, text = "Salir", command = salir) 
+
+ventana.mainloop()
