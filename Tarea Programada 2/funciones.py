ea84af459c056644f5c947656452aea30705486f
abicoto24 <164296369+abicoto24@users.noreply.github.com>
2026-05-18 19:47:37 -0600

    Creación de botones: Registrar, Limpiar y Regresar
    con respecto al import os, sirve para desactivar las opciones 3, 4 y 6 a menos de que ya exista una base de datos.

--- Tarea Programada 2/funciones.py
+++ Tarea Programada 2/funciones.py
@@ -13,6 +13,74 @@ def insertarDonador():
     campoCedula = tk.Entry(ventanaInsertarDonador)
     campoCedula.pack()
 
+    etiquetaNombre = tk.Label(ventanaInsertarDonador, text = "Nombre completo: ")
+    etiquetaNombre.pack()
+
+    campoNombre = tk.Entry(ventanaInsertarDonador)
+    campoNombre.pack()
+
+    etiquetaFecha = tk.Label(ventanaInsertarDonador, text = "Fecha de Nacimiento: ")
+    etiquetaFecha.pack()
+
+    campoFecha = tk.Entry(ventanaInsertarDonador)
+    campoFecha.pack()
+
+    tipoSangreVar = tk.StringVar()
+    tipoSangreVar.set("O+")
+
+    listaTipoSangre = tk.OptionMenu(ventanaInsertarDonador, tipoSangreVar, *tiposSangre)
+    listaTipoSangre.pack()
+
+    sexoVar = tk.BooleanVar()
+    sexoVar.set(True)
+
+    radioMasculino = tk.Radiobutton(ventanaInsertarDonador, text = "Masculino", variable = sexoVar, value = True)
+    radioMasculino.pack()
+
+    radioFemenino = tk.Radiobutton(ventanaInsertarDonador, text = "Femenino", variable = sexoVar, value = False)
+    radioFemenino.pack() 
+
+    etiquetaPeso = tk.Label(ventanaInsertarDonador, text = "Peso (kg): ")
+    etiquetaPeso.pack()
+
+    campoPeso = tk.Entry(ventanaInsertarDonador)
+    campoPeso.pack()
+
+    etiquetaTel = tk.Label(ventanaInsertarDonador, text = "Teléfono: ")
+    etiquetaTel.pack()
+
+    campoTel = tk.Entry(ventanaInsertarDonador)
+    campoTel.pack()
+
+    etiquetaCorreo = tk.Label(ventanaInsertarDonador, text = "Correo: ")
+    etiquetaCorreo.pack()
+
+    campoCorreo = tk.Entry(ventanaInsertarDonador)
+    campoCorreo.pack()
+
+    def registrar():
+        print()
+
+    def limpiar():
+        campoCedula.delete(0, tk.END)
+        campoNombre.delete(0, tk.END)
+        campoFecha.delete(0, tk.END)
+        tipoSangreVar.set("O+")
+        sexoVar.set(True)
+        campoPeso.delete(0, tk.END)
+        campoTel.delete(0, tk.END)
+        campoCorreo.delete(0, tk.END)
+    
+    botonRegistrar = tk.Button(ventanaInsertarDonador, text="Registrar", command=registrar)
+    botonRegistrar.pack()
+
+    botonLimpiar = tk.Button(ventanaInsertarDonador, text="Limpiar", command=limpiar)
+    botonLimpiar.pack()
+
+    botonRegresar = tk.Button(ventanaInsertarDonador, text="Regresar", command=ventanaInsertarDonador.destroy)
+    botonRegresar.pack()
+
+
 def generarDonadores():
     print()
 
